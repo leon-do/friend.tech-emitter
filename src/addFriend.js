43c19a27ead92313bc3c324ebb75aef625c083e1
leon-do <leondo123@gmail.com>
2023-08-24 11:40:03 -0400

    1k

--- src/addFriend.js
+++ src/addFriend.js
@@ -11,7 +11,7 @@ export default async function addFriend(_twitterUsername) {
   // read friends.txt
   const friends = fs.readFileSync(path, "utf8");
   // if list is long enough
-  if (friends.split("\n").length > 333) {
+  if (friends.split("\n").length > 1000) {
     try {
       // send email
       await resend.emails.send({

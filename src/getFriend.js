d6a20d911cd961886ae03fb411884b18792536c4
leon-do <leondo123@gmail.com>
2023-08-25 16:20:28 -0400

    delay friend

--- src/getFriend.js
+++ src/getFriend.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // getFriend("0xb74246Aa1d35ff55A25B82408560765213212DEc");
 export default async function getFriend(address) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const friend = await axios
     .get(`https://prod-api.kosetto.com/users/${address}`)
     .then((res) => res.data)
